331080f714cc21a7d83b2beb3f6e77fe9d30bd1e
leVirve <gae.m.project@gmail.com>
2017-01-16 20:38:19 +0800

    move evaluion into evaluate module

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import argparse
-import pickle
-import re
 
 from preprocess import MSRII
 from preprocess.c3d import C3DFeatureNet
-from preprocess.video import Video
-from evaluate.utils import non_maximum_suppression
+from network.trainer import Trainer
+from network.model import FC4Net
+from evaluate.evaluator import ProposalEvaluator
 
 
 def extract_feature():
@@ -14,46 +13,20 @@ def extract_feature():
     c3dnet.load()
     c3dnet.start(dataset)
 
-def test_proposal():
-    from network.model import FC4Net
 
-    with open('data/outputs/training_info.pkl', 'rb') as f:
-        results = pickle.load(f)
+def train_models(args):
+    trainer = Trainer(feature_file='data/features/MSRII-c3d-features.h5')
+    trainer.run(args)
+    trainer.summary()
 
-    test_videos_keys = results['exclude_features_keys']
-    find_vid = re.compile('features_(\d+).avi')
-    video_ids = [int(find_vid.search(k).group(1)) for k in test_videos_keys]
 
+def generate_proposal():
     dataset = MSRII.Dataset('/data-disk/MSRII/')
-    c3dnet = C3DFeatureNet(feature_file=None)
-    c3dnet.load()
     fc4net = FC4Net()
     fc4net.load_weights('data/weights/FC4Net_weights.h5')
 
-    results = {}
-    for vid in video_ids:
-        video_meta = dataset.video_metas[vid]
-        video = Video('/data-disk/MSRII/videos/' + video_meta.name)
-        y = c3dnet.extract_feature(video)
-        pred = fc4net.predict(y, batch_size=32)
-
-        predict = non_maximum_suppression(pred, tol_frames=16)
-
-        seg_metas = dataset.video_metas[vid].seg_metas
-        target = []
-        for seg_meta in seg_metas:
-            s = seg_meta['start']
-            t = seg_meta['duration']
-            target.append((s, s + t))
-
-        results[video_meta.name] = {
-            'ground_truth': target,
-            'predict': predict
-        }
-
-    with open('data/outputs/predicted_proposal.pkl', 'wb') as f:
-        pickle.dump(results, f, protocol=2)  # For Python2
-
+    evaluatoor = ProposalEvaluator(dataset)
+    evaluatoor.eval(fc4net)
     # calc tIOU
 
 
@@ -75,9 +48,6 @@ if __name__ == '__main__':
     if args.extract:
         extract_feature()
     elif args.train:
-        from network.trainer import Trainer
-        trainer = Trainer('data/features/MSRII-c3d-features.h5')
-        trainer.run(args)
-        trainer.summary()
+        train_models(args)
     elif args.eva:
-        test_proposal()
+        generate_proposal()
